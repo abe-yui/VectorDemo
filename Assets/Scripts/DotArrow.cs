9830f168e2540954cb7f67a018f5172275c736af
A601-01 <tnk.dat.class@gmail.com>
2020-10-22 11:16:46 +0900

    コメント追加

--- Assets/Scripts/DotArrow.cs
+++ Assets/Scripts/DotArrow.cs
@@ -30,15 +30,18 @@ public class DotArrow : MonoBehaviour
         var count = Physics.RaycastNonAlloc(ray, hits, float.PositiveInfinity, planeLayer);
         if (count > 0)
         {
-            var target = targetArrow.toPosition - targetArrow.fromPosition;
+            var target = targetArrow.toPosition - targetArrow.fromPosition;//対象の矢印の先端座標-対象の矢印の根座標
             var current = hits[0].point - targetArrow.fromPosition;
-            var dot = Vector3.Dot(target, current);
+            var dot = Vector3.Dot(target, current);//コサイン
+
 
             var dotVector = target.normalized * dot;
+            //magnitude(ベクトルの長さ)を1としたベクトル（読み取り専用）//この場合はtarget
 
-            currentArrow.fromPosition = targetArrow.fromPosition;
-            currentArrow.toPosition = targetArrow.fromPosition + dotVector;
-            currentArrow.UpdateArrow();
+
+            currentArrow.fromPosition = targetArrow.fromPosition;//currentの根座標＝targetの根座標
+            currentArrow.toPosition = targetArrow.fromPosition + dotVector;//currentの先端座標＝targetの根座標+dotVector
+            currentArrow.UpdateArrow();//読み込み
         }
     }
 }
